2b3fcaa91132e1da96c7483ca1a9e3b24f5428c1
Zdenek L <66368980+Luzkix@users.noreply.github.com>
2024-02-03 18:53:17 +0100

    Added spring boot security dependency (and exluding it in main class). Fixed non working test context by specifying unit4 tests instead of jupiter tests and by adding testing H2 database

--- src/test/java/com/example/bitcoinpricechecker/BitcoinPriceCheckerApplicationTests.java
+++ src/test/java/com/example/bitcoinpricechecker/BitcoinPriceCheckerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.bitcoinpricechecker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BitcoinPriceCheckerApplicationTests {
+public class BitcoinPriceCheckerApplicationTests {
 
   @Test
-  void contextLoads() {
+  public void contextLoads() {
   }
 
 }
